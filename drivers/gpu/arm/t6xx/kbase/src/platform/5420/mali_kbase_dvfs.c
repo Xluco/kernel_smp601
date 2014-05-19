fd9a11f9e4cfdf926392694abed1019b5f864827
halaszk <halaszk@deathlord.eu>
2014-05-19 15:04:03 +0200

    MALI: added 800MHz OC freq
    Conflicts:
    	arch/arm/mach-exynos/include/mach/asv-exynos5420.h
    	drivers/gpu/arm/t6xx/kbase/src/platform/5420/mali_kbase_dvfs.c

--- drivers/gpu/arm/t6xx/kbase/src/platform/5420/mali_kbase_dvfs.c
+++ drivers/gpu/arm/t6xx/kbase/src/platform/5420/mali_kbase_dvfs.c
@@ -76,10 +76,10 @@
 
 #if defined(CONFIG_EXYNOS_THERMAL)
 #include <mach/tmu.h>
-#define GPU_MAX_CLK 733
-#define GPU_THROTTLING_90_95 600
-#define GPU_THROTTLING_95_100 533
-#define GPU_THROTTLING_100_105 480
+#define GPU_MAX_CLK 800
+#define GPU_THROTTLING_90_95 800
+#define GPU_THROTTLING_95_100 733
+#define GPU_THROTTLING_100_105 666
 #define GPU_THROTTLING_105_110 266
 #define GPU_TRIPPING_110 177
 #endif
@@ -125,21 +125,22 @@ typedef struct _mali_dvfs_info{
 static mali_dvfs_info mali_dvfs_infotbl[] = {
 	{812500, 100, 0, 40, 0, 133000, 83000, 100000},
 	{812500, 177, 41, 50, 0, 133000, 83000, 100000},
-	{850000, 266, 51, 60, 0, 400000, 222000, 250000},
-	{900000, 350, 61, 65, 0, 667000, 333000, 250000},
-    {950000, 420, 66, 70, 0, 733000, 400000, 650000},
-	{987500, 480, 71, 75, 0, 733000, 400000, 650000},
-	{1025000, 533, 76, 80, 0, 733000, 400000, 650000},
-	{1050000, 600, 81, 85, 0, 800000, 600000, 1200000},
-    {1062500, 667, 86, 94, 0, 800000, 600000, 1300000},
-    {1100000, 733, 95, 99, 0, 933000, 600000, 1400000},
+	{862500, 266, 51, 60, 0, 400000, 222000, 250000},
+	{912500, 350, 61, 70, 0, 667000, 333000, 250000},
+	{962500, 420, 71, 80, 0, 733000, 400000, 650000},
+	{1000000, 480, 81, 85, 0, 733000, 400000, 650000},
+	{1037500, 533, 86, 90, 0, 733000, 400000, 650000},
+	{1050000, 600, 91, 92, 0, 800000, 600000, 1200000},
+        {1075000, 666, 93, 94, 0, 800000, 600000, 1300000},
+        {1100000, 733, 95, 96, 0, 933000, 600000, 1400000},
+        {1100000, 800, 97, 99, 0, 933000, 600000, 1600000},
 };
 
 #define MALI_DVFS_STEP	ARRAY_SIZE(mali_dvfs_infotbl)
 
 unsigned int dvfs_step_min = 0;
-unsigned int dvfs_step_max = 10;
-unsigned int dvfs_step_max_minus1 = 667;
+unsigned int dvfs_step_max = 11;
+unsigned int dvfs_step_max_minus1 = 480;
 unsigned int cur_gpu_freq = 0;
 
 #ifdef CONFIG_MALI_T6XX_DVFS
@@ -169,7 +170,7 @@ static void update_time_in_state(int level);
 /*dvfs status*/
 static mali_dvfs_status mali_dvfs_status_current;
 #ifdef MALI_DVFS_ASV_ENABLE
-static const unsigned int mali_dvfs_vol_default[] = { 812500, 812500, 850000, 900000, 950000, 987500, 1025000, 1050000, 1062500, 1100000};
+static const unsigned int mali_dvfs_vol_default[] = { 812500, 812500, 862500, 912500, 962500, 1000000, 1037500, 1050000, 1075000, 1100000, 1100000};
 
 ssize_t hlpr_get_gpu_volt_table(char *buf)
 {
@@ -239,6 +240,7 @@ void hlpr_set_min_max_G3D(unsigned int min, unsigned int max)
 	int tbl7[8] = { 95, 90, 85, 75, 65, 55, 50, 40 };
 	int tbl8[9] = { 95, 90, 80, 70, 65, 60, 55, 50, 40 };
 	int tbl9[10] = { 95, 85, 80, 70, 65, 60, 55, 50, 45, 40 };
+        int tbl10[11] = { 95, 90, 85, 80, 70, 65, 60, 55, 50, 45, 40 };
 	
 	for (i = 0; i < MALI_DVFS_STEP; i++)
 	{
@@ -271,6 +273,8 @@ void hlpr_set_min_max_G3D(unsigned int min, unsigned int max)
 		hlpr_set_gpu_gov_table(tbl8);
 	else if (dvfs_step_max - dvfs_step_min == 10)
 		hlpr_set_gpu_gov_table(tbl9);
+        else if (dvfs_step_max - dvfs_step_min == 11)
+                hlpr_set_gpu_gov_table(tbl10);
 }
 
 static int mali_dvfs_update_asv(int cmd)
