08e05ba3b7f44c05bcdaa148daeed72925a5a9b6
Steve Muckle <smuckle@codeaurora.org>
2014-07-14 03:19:29 -0700

    sched: fix race between try_to_wake_up() and move_task()
    Until a task's state has been seen as interruptible/uninterruptible
    and it is no longer on_cpu, it is possible that the task may move
    to another CPU (load balancing may cause this). Here is an example
    where the race condition results in incorrect operation:
    
    - cpu 0 calls put_prev_task on task A, task A's state is TASK_RUNNING
    - cpu 0 runs task B, which attempts to wake up A
    - cpu 0 begins try_to_wake_up(), recording src_cpu for task A as cpu 0
    - cpu 1 then pulls task A (perhaps due to idle balance)
    - cpu 1 runs task A, which then sleeps, becoming INTERRUPTIBLE
    - cpu 0 continues in try_to_wake_up(), thinking task A's previous
      cpu is 0, where it is actually 1
    - if select_task_rq returns cpu 0, task A will be woken up on cpu 0
      without properly updating its cpu to 0 in set_task_cpu()
    
    CRs-Fixed: 665958
    Change-Id: Icee004cb320bd8edfc772d9f74e670a9d4978a99
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	kernel/sched/core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1599,6 +1599,8 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 
 	smp_wmb();
 	raw_spin_lock_irqsave(&p->pi_lock, flags);
+	src_cpu = cpu = task_cpu(p);
+
 	if (!(p->state & state))
 		goto out;
 
@@ -1640,7 +1642,10 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 		p->sched_class->task_waking(p);
 
 	cpu = select_task_rq(p, SD_BALANCE_WAKE, wake_flags);
-	if (task_cpu(p) != cpu) {
+
+	/* Refresh src_cpu as it could have changed since we last read it */
+	src_cpu = task_cpu(p);
+	if (src_cpu != cpu) {
 		wake_flags |= WF_MIGRATED;
 		set_task_cpu(p, cpu);
 	}
