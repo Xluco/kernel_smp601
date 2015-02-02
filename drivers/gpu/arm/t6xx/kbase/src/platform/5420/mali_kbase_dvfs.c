a3bcfc1f71d10c46b43741714c6ef99b40abe747
Xluco <git@phugh.es>
2015-02-02 16:11:45 +0000

    Mali: dvfs_step_max_minus1 = 733

--- drivers/gpu/arm/t6xx/kbase/src/platform/5420/mali_kbase_dvfs.c
+++ drivers/gpu/arm/t6xx/kbase/src/platform/5420/mali_kbase_dvfs.c
@@ -140,7 +140,7 @@ static mali_dvfs_info mali_dvfs_infotbl[] = {
 
 unsigned int dvfs_step_min = 0;
 unsigned int dvfs_step_max = 11;
-unsigned int dvfs_step_max_minus1 = 480;
+unsigned int dvfs_step_max_minus1 = 733;
 unsigned int cur_gpu_freq = 0;
 
 #ifdef CONFIG_MALI_T6XX_DVFS
